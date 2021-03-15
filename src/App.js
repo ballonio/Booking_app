2cbbd22d49a015c0a83ad1e8207ee1e3430647dd
Ballonio <77andreypetruk@gmail.com>
2021-03-15 14:44:12 +0100

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react';
 
-function App() {
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Reservations/>
+    <Days/>
     </div>
   );
 }
+
+function Days(props) {
+
+  const getDays = (num) => {
+    const array = []
+
+    for(var i = 1; i <= num; i++){
+      array.push(<button onClick={ () => props.toggleReserv } >{i}</button>)
+    }
+    return array
+  }
+    return(
+      <div>
+        <h4>January</h4>
+        {getDays(30)}
+      </div>
+    )
+  }                                                                                    //// chyba żle przekazuję propsa :(
+
+function Reservations() {
+  const [show, setShow] = useState(false);
+  const [days, setDays] = useState([{dayId: 1, dayLabel: "1",  reservations:  "Jan Kowalski"}, 
+  { dayId: 2, dayLabel: "2", reservations:  "Bartek Bojanek" }]);
+  const toggleReserv = () => setShow({show: !show});
+ return (
+ <div>
+   
+   <button  onClick={ () => toggleReserv() }>1</button> 
+   <ul>
+     {show
+     ? days.map(day => (<li key={day.dayId}>{day.dayLabel} {day.reservations}</li>)) 
+     : "" }
+  </ul>
+ </div>
+ )                                                                                     //Rezerwacje nie znikają po kolejnym clicku                                               
+}
+
+
 
 export default App;
